ebf077f071f477e59c1370dbc66dfe11267d2ee6
Rahul Kadukar <rahulkadukar@yahoo.com>
2019-04-08 23:39:57 -0400

    eslint fix

--- headerFile.js
+++ headerFile.js
@@ -4,18 +4,18 @@ const { readFileSync, writeFileSync } = require('fs')
 const assertCheck = fnData => assert(fnData.errNo === 0, fnData.errMsg)
 
 const formatDate = (date) => {
-  let d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear().toString().substring(2,4)
+  const d = new Date(date)
+  let month = `${d.getMonth() + 1}`
+  let day = `${d.getDate()}`
+  const year = d.getFullYear().toString().substring(2, 4)
 
-  if (month.length < 2) month = '0' + month
-  if (day.length < 2) day = '0' + day
+  if (month.length < 2) month = `0${month}`
+  if (day.length < 2) day = `0${day}`
 
   return [year, month, day].join('')
 }
 
-const fileName = 'hwinfo_' + formatDate(new Date()) + '.log'
+const fileName = `hwinfo_${formatDate(new Date())}.log`
 
 const fileContents = readFile(fileName)
 assertCheck(fileContents)
@@ -33,7 +33,7 @@ function writeHeader(inputData) {
 
 function readFile(inputFile) {
   let fileData
-  let outputData = {}
+  const outputData = {}
   outputData.errMsg = ''
   outputData.errNo = 0
 
@@ -41,15 +41,14 @@ function readFile(inputFile) {
     fileData = readFileSync(inputFile, 'utf-8')
   } catch (err) {
     consoleError(`ERROR => Unable to read file the ${inputFile}`)
-    consoleError(`USAGE => file should be placed in the temp folder`)
+    consoleError('USAGE => file should be placed in the temp folder')
     outputData.errMsg = `ERROR => Unable to read the file ${inputFile}`
     outputData.errNo = -1
   } finally {
     if (fileData) {
-      outputData.data = fileData.split(/\r\n|\r|\n/).filter((r) => r.length > 0)
+      outputData.data = fileData.split(/\r\n|\r|\n/).filter(r => r.length > 0)
     }
 
     return outputData
   }
 }
-
